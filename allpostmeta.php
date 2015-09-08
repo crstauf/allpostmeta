555b375b16af0d12384820d9c7b5ffce7160162b
CALEB STAUFFER <develop+github@calebstauffer.com>
2015-09-08 15:39:54 -0400

    display fixes and integration with Query Monitor

--- allpostmeta.php
+++ allpostmeta.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: All Post Meta
-Plugin URI: 
-Description: Shows all post meta, even hidden ones...
-Version: 0.0.1
+Plugin URI:
+Description: Shows all post information: post object, attached images, (hidden) meta data
+Version: 0.0.2
 Author: Caleb Stauffer
 Author URI: http://develop.calebstauffer.com
 */
@@ -13,8 +13,28 @@ new allpostmeta;
 class allpostmeta {
 
 	function __construct() {
-		if (is_admin()) add_action('add_meta_boxes',array(__CLASS__,'add_meta_boxes'));
+		add_filter('qm/collectors',array(__CLASS__,'register_qm_collector'),20,2);
+		add_filter('qm/outputter/html',array(__CLASS__,'register_qm_output'),76,2);
+		add_action('plugins_loaded',array('allpostmeta','init'));
 	}
+
+	public static function init() {
+		if (!class_exists('QueryMonitor') && is_admin())
+			add_action('add_meta_boxes',array(__CLASS__,'add_meta_boxes'));
+	}
+
+		public static function register_qm_collector( array $collectors, QueryMonitor $qm ) {
+			$collectors['allpostmeta'] = new CSS_QM_Collector_allpostmeta;
+			return $collectors;
+		}
+
+		public static function register_qm_output( array $output, QM_Collectors $collectors ) {
+			require_once plugin_dir_path(__FILE__) . 'qm-output.php';
+			if ( $collector = QM_Collectors::get( 'allpostmeta' ) ) {
+				$output['allpostmeta'] = new CSS_QM_Output_Html_allpostmeta( $collector );
+			}
+			return $output;
+		}
 
 	public static function add_meta_boxes() {
 		add_meta_box('allpostmeta','All Post Meta',array(__CLASS__,'meta_box'),'','advanced');
@@ -24,24 +44,25 @@ class allpostmeta {
 		echo '<table class="code" cellspacing="0" cellpadding="0" style="width: 100%;">';
 		echo '<tbody>';
 		$i = 0;
-		foreach (get_post_custom($post->ID) as $k => $array) {
+
+		foreach ($post as $k => $v) {
 			$i++;
-			if (1 == count($array)) $v = $array[0];
-			else $v = print_r($array,true);
+			$value = self::value($v);
+			if ('post_content' == $k) $v = '<textarea rows="2" readonly="readonly" style="width: 98%; max-width: 98%; padding: 3px 5px; font-size: 11px; resize: vertical;">' . $v . '</textarea>';
 			echo '<tr style="' . (0 == ($i % 2) ? ' background-color: #F6F6F6;' : '') . '">';
 				echo '<td style="width: 45%; padding: 5px;">' . $k . '</td>';
-				echo '<td style="width: 45%; padding: 5px;">' . htmlentities($v) . '</td>';
+				echo '<td style="width: 45%; padding: 5px;"><div style="overflow: auto; max-width: 100%; max-height: 300px;">' . (is_array($value) || is_object($value) ? '<pre>' . print_r($value,true) . '</pre>' : $value) . '</div></td>';
 			echo '</tr>';
 		}
 
 		echo '<tr><td colspan="2">&nbsp;</td></tr>';
-		
-		foreach ($post as $k => $v) {
+
+		foreach (get_post_custom($post->ID) as $k => $array) {
 			$i++;
-			if ('post_content' == $k) $v = '<textarea rows="2" readonly="readonly" style="width: 98%; max-width: 98%; padding: 3px 5px; font-size: 11px; resize: vertical;">' . $v . '</textarea>';
+			$array = self::value($array);
 			echo '<tr style="' . (0 == ($i % 2) ? ' background-color: #F6F6F6;' : '') . '">';
 				echo '<td style="width: 45%; padding: 5px;">' . $k . '</td>';
-				echo '<td style="width: 45%; padding: 5px;">' . $v . '</td>';
+				echo '<td style="width: 45%; padding: 5px;"><div style="overflow: auto; max-width: 100%; max-height: 300px;"><pre>' . print_r($array,true) . '</pre></div></td>';
 			echo '</tr>';
 		}
 
@@ -58,7 +79,7 @@ class allpostmeta {
 						$terms = get_the_terms($post->ID,$slug);
 						if (is_array($terms) && count($terms)) {
 							foreach ($terms as $term) $output[] = $term->name;
-							echo implode(', ',$output);
+							echo implode('", "',$output) . '"';
 						}
 					 echo '</td>';
 				echo '</tr>';
@@ -69,7 +90,41 @@ class allpostmeta {
 		echo '</tbody>';
 		echo '</table>';
 	}
-	
+
+	public static function value($value,$entities = true) {
+		$value = maybe_unserialize($value);
+		if (is_array($value) || is_object($value))
+			foreach ($value as $k => $v)
+				$value[$k] = self::value($v);
+		else if (true === $entities) $value = htmlentities($value);
+		return $value;
+	}
+
+}
+
+if (class_exists('QM_Collector')) {
+	class CSS_QM_Collector_allpostmeta extends QM_Collector {
+
+		public $id = 'allpostmeta';
+
+		public function name() {
+			return __( 'All Post Meta', 'query-monitor' );
+		}
+
+		public function __construct() {
+
+			global $wpdb;
+
+			parent::__construct();
+
+		}
+
+		public function process() {
+			$this->data['allpostmeta'] = array('a','b','c','d');
+
+		}
+
+	}
 }
 
 ?>
---
+++ qm-output.php
@@ -0,0 +1,93 @@
+<?php
+
+class CSS_QM_Output_Html_allpostmeta extends QM_Output_Html {
+
+	public function __construct( QM_Collector $collector ) {
+		parent::__construct( $collector );
+		add_filter( 'qm/output/menus', array( $this, 'admin_menu' ), 61 );
+	}
+
+	public function output() {
+		global $wp_the_query;
+
+		if (!isset($wp_the_query) || !is_object($wp_the_query->post) || !count($wp_the_query->post)) global $post;
+		else $post = $wp_the_query->post;
+
+		$meta = array();
+
+		foreach ($post as $key => $value)
+			$meta['Object'][$key] = $value;
+
+		if ($taxes = get_object_taxonomies(get_post_type($post),'objects')) {
+			foreach ($taxes as $slug => $tax) {
+				$terms = get_the_terms($post->ID,$slug);
+				if (is_array($terms)) {
+					$meta['Taxonomies'][$slug] = '';
+					foreach ($terms as $term)
+						if ('' !== $slug)
+							$meta['Taxonomies'][$slug] .= '"' . $term->name . '", ';
+					if ('' !== $meta['Taxonomies'][$slug])
+						$meta['Taxonomies'][$slug] = substr($meta['Taxonomies'][$slug],0,strlen($meta['Taxonomies'][$slug]) - 2);
+				}
+			}
+		}
+
+		$custom = get_post_custom($post->ID);
+		ksort($custom);
+		foreach ($custom as $key => $value)
+			$meta['Custom Fields'][$key] = $value;
+
+			$imgs = new WP_Query(array(
+				'post_type' => 'attachment',
+				'posts_per_page' => -1,
+				'post_status' => 'inherit',
+				'post_parent' => $post->ID,
+			));
+
+			if ($imgs->have_posts()) {
+				$orig = $post;
+				$meta['Attached Images'][' '] = '';
+				while ($imgs->have_posts()) {
+					$imgs->the_post();
+					$thumb = wp_get_attachment_image_src(get_the_ID(),'thumbnail');
+					$meta['Attached Images'][' '] .= '<a href="' . get_edit_post_link(get_the_ID()) . '" target="_blank"><img src="' . $thumb[0] . '" width="' . $thumb[1] . '" height="' . $thumb[2] . '" alt="' . get_the_ID() . '" style="width: 50px; height: auto;" /></a>';
+				}
+				$post = $orig;
+				wp_reset_postdata();
+			}
+
+		echo '<div id="' . esc_attr( $this->collector->id() ) . '" class="qm qm-full">';
+
+		echo '<table cellspacing="0">';
+		echo '<thead>';
+		echo '<tr>';
+		echo '<th colspan="3">All Post Meta</th>';
+		echo '</tr>';
+		echo '</thead>';
+		echo '<tbody>';
+
+		foreach ($meta as $name => $group) {
+			$first = true;
+
+			echo '<tr><td rowspan="' . count($meta[$name]) . '">' . $name . '</td>';
+
+			foreach ($group as $key => $value) {
+				$v = allpostmeta::value($value,('Attached Images' === $name ? false : true));
+				if (true !== $first) echo '<tr>';
+					if ('' !== trim($key)) echo '<td>' . $key . '</td>';
+					echo '<td' . ('' === trim($key) ? ' colspan="3"' : '') . '><div style="overflow: auto; max-width: 100%; max-height: 300px;">' . (is_array($v) || is_object($v) ? '<pre>' . print_r($v,true) . '</pre>' : $v) . '</div></td>';
+				echo '</tr>';
+				$first = false;
+			}
+		}
+
+		echo '</tbody>';
+		echo '</table>';
+
+		echo '</div>';
+
+	}
+
+}
+
+?>
